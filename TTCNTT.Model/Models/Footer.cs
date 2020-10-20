da10980e00e26c1c3d48b9ae169e3fac930e8a78
DESKTOP-920LDA3\lenam <namlth9588@gmail.com>
2020-10-20 11:10:01 +0700

    test4

---
+++ TTCNTT.Model/Models/Footer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TTCNTT.Model.Models
+{
+    [Table("Footers")]
+    internal class Footer
+    {
+        [Key]
+        public string ID { set; get; }
+
+        [Required]
+        public string Content { set; get; }
+    }
+}
---
+++ TTCNTT.Model/Models/MenuGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTCNTT.Model.Models
+{
+    [Table("MenuGroups")]
+    class MenuGroup
+    {
+        [Key]
+        public int ID { set; get; }
+        
+        [Required]
+        public string Name { set; get; }
+    }
+}
